e75ddba40e64f7953fc31ca39b10d89f1225fd5f
geobarteam <gvd8@hotmail.com>
2017-01-12 16:25:12 +0100

    final

--- startServices.ps1
+++ startServices.ps1
@@ -1,3 +1,4 @@
 docker exec -d backend1 /bin/bash -c "dotnet run"
 docker exec -d backend2 /bin/bash -c "dotnet run"
 docker exec -d backend3 /bin/bash -c "dotnet run"
+#docker exec -d frontend1 /bin/bash -c "dotnet run"
